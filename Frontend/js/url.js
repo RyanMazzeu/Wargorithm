4711b44b50254b185ced80d93237fe7401909440
RyanMazzeu <ryanmazzeu111@gmail.com>
2025-04-23 10:25:01 -0300

    Ajustando logo da community

--- Frontend/js/url.js
+++ Frontend/js/url.js
@@ -1,5 +1,5 @@
 // url.js
-//const API_URL = "http://localhost:5000";
-const API_URL = "https://wargorithm.onrender.com";
+const API_URL = "http://localhost:5000";
+//const API_URL = "https://wargorithm.onrender.com";
 
 export default API_URL;
